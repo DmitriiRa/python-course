fbe3e131fd511aeddc20b1b7b594f8b0ae5441e1
DmitriiRa <ramir67@rambler.ru>
2015-10-15 01:24:17 +0300

    modified 0 task from 2 homwwork

--- homeworks/homework2/task0.py
+++ homeworks/homework2/task0.py
@@ -1,11 +1,11 @@
 def plural(number, words):
 
     if ((number % 100 != 11) & ((number % 10) == 1)):
-        print(number, words[0])
-    elif((4 < number % 100 < 21) == False & (1 < (number % 100) < 5)):
-        print(number, words[1])
+        print(number, ''.join(words[0]))
+    elif(((4 < number % 100 < 21) == False) & (1 < (number % 100) < 5)):
+        print(number, ''.join(words[1]))
     else:
-        print(number, words[2])
+        print(number, ''.join(words[2]))
 
 
 word = input()
