852ed61f9419c3bec92b027bd8ef3bb06e9a0d25
DmitriiRa <Ramir67@rambler.ru>
2015-12-07 23:58:18 +0300

    Update task4.py

--- homeworks/homework5/task4.py
+++ homeworks/homework5/task4.py
@@ -3,32 +3,26 @@ import re
 
 
 def check(string):
-    string = re.findall("\S+", string) # избавились от пробелов и табуляции
-    if (re.match("\.", string[0]) != None): # отбросили слова начинающиеся с "."
+    string = re.findall("\S+", string)
+    if (re.match("\.", string[0]) != None):
         return
-    if (re.findall("\.\.", string[0]) != []): # отбросили слова с ".."
+    if (re.findall("\.\.", string[0]) != []):
         return
     return string
 
 
-f = open('temp.txt', 'w')
-a = sys.stdin.read()
-f.write(a) 
-f.close()
+code = sys.stdin.read()
 
-
-with open("temp.txt") as f: 
-    j = 0
-    data = f.read().split("\n")
-    for string in data:
-        newstring = string.split("#")[0]
-        par = re.findall("(.+) = ", newstring) # par - все  что перед =
-        j += 1
-        if par != []: # если в строке есть присвоение
-            a = par[0].split(", ") # разбили строку по запятым
-            print(j, end = " ")
-            for i in range(len(a)): 
-                if check(a[i]) != None:
-                    print(check(a[i])[0], end = " ")
-            print()
-            
+j = 0
+data = code.split("\n")
+for string in data:
+    newstring = string.split("#")[0]
+    par = re.findall("(.+) = ", newstring)
+    j += 1
+    if par != []:
+        a = par[0].split(", ")
+        print(j, end = " ")
+        for i in range(len(a)):
+            if check(a[i]) != None:
+                print(check(a[i])[0], end = " ")
+        print()
